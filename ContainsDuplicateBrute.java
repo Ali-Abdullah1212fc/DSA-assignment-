2b88aada50d73be09da48b8a771163f7437878ce
Ali-Abdullah1212fc <l1f24bsse0332@ucp.edu.pk>
2026-06-28 22:26:47 +0500

    Add files via upload

---
+++ ContainsDuplicateBrute.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+
+public class ContainsDuplicateBrute {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == nums[i + 1])
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,4,3,5,3,2,1,3,6};
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
